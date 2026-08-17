21bcc9859fd028ac693410c4b861d38d332fa291
Shahzain Ahmed <shahzainahmed57@gmail.com>
2026-08-17 21:01:46 +0500

    [FIX] Unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:hotel_booking_app/resources/colors/app_colors.dart';
 import 'package:hotel_booking_app/resources/theme/app_themes.dart';
 import 'package:hotel_booking_app/view/home/home_screen.dart';
 
